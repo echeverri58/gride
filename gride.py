02737a4e0f01cfeb5b65d579c69ef8821915691d
echeverri58 <85091279+echeverri58@users.noreply.github.com>
2023-12-03 20:00:36 -0500

    Update gride.py

--- gride.py
+++ gride.py
@@ -1,14 +1,15 @@
 import streamlit as st
 import plotly.express as px
-import numpy as np
 
-def generate_random_bar_chart():
-    # Generar datos aleatorios para un gráfico de barras
-    categories = [f"Categoria {i}" for i in range(1, 11)]
-    values = np.random.randint(1, 20, size=10)
+def generate_custom_bar_chart():
+    # Datos personalizados para un gráfico de barras
+    data = {
+        'Categoría': ['A', 'B', 'C', 'D', 'E'],
+        'Valor': [15, 8, 12, 6, 10]
+    }
 
     # Crear gráfico de barras con Plotly Express
-    fig = px.bar(x=categories, y=values, labels={'x': 'Categoría', 'y': 'Valor'}, title='Gráfico de Barras Aleatorio')
+    fig = px.bar(data, x='Categoría', y='Valor', labels={'x': 'Categoría', 'y': 'Valor'}, title='Gráfico de Barras Personalizado')
 
     return fig
 
@@ -27,16 +28,17 @@ def main():
             col1, col2, col3, col4 = st.columns(4)
             
             with col1:
-                st.plotly_chart(generate_random_bar_chart(), use_container_width=True)
+                st.plotly_chart(generate_custom_bar_chart(), use_container_width=True)
 
             with col2:
-                st.plotly_chart(generate_random_bar_chart(), use_container_width=True)
+                st.plotly_chart(generate_custom_bar_chart(), use_container_width=True)
 
             with col3:
-                st.plotly_chart(generate_random_bar_chart(), use_container_width=True)
+                st.plotly_chart(generate_custom_bar_chart(), use_container_width=True)
 
             with col4:
-                st.plotly_chart(generate_random_bar_chart(), use_container_width=True)
+                st.plotly_chart(generate_custom_bar_chart(), use_container_width=True)
 
 if __name__ == "__main__":
     main()
+
